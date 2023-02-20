fc04306b5b31baf34e5580f44235e0a9e0183ec8
Joanna Bialkowska <yoanna.bialkowska@gmail.com>
2023-02-20 08:21:34 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -1,42 +1,108 @@
-console.log("Hello Devs! Little fun-fact for today: the weight of Milky Way galaxy is estimated around 1,5 trillion solar masses! ;>")
+{
+    const welcome = () => {
+        console.log(
+            "Hello Devs! Little fun-fact for today: the weight of Milky Way galaxy is estimated around 1,5 trillion solar masses! ;>"
+        );
+    };
 
-let buttonHideSquirrel = document.querySelector(".js-buttonHideSquirrel")
-let imageChange = document.querySelector(".js-photo")
-let catName = document.querySelector(".js-paragraphCat")
-let body = document.querySelector(".js-body")
-let background = document.querySelector(".js-background")
-let headline = document.querySelectorAll(".js-sectionHeadline")
-let tableHeader = document.querySelectorAll(".js-tableHeader")
-let tableCellBackground = document.querySelectorAll(".js-tableCell")
-let hyperlink = document.querySelectorAll(".js-hyperlink")
-let endText = document.querySelector(".js-footerText")
-let decoration = document.querySelector(".js-sectionDecoration")
-let buttonSwitchMode = document.querySelector(".js-buttonSwitchMode")
+    const imageChanging = () => {
+        let imageChange = document.querySelector(".js-photo");
 
-buttonHideSquirrel.addEventListener("click", () => {
-    buttonHideSquirrel.remove();
-    catName.innerText = "Przyszywany brat - Rumcajs. Postrach wiewiórek!";
-    decoration.classList.add("lineDecoration");
+        switch (imageChange.getAttribute("src")) {
+            case "https://i.postimg.cc/LXcFfSF2/Squirrel.png":
+                imageChange.setAttribute(
+                    "src",
+                    "https://i.postimg.cc/NG7Bj51Q/Rumcajs1.jpg"
+                );
+                break;
+        }
+    };
 
-    switch (imageChange.getAttribute("src")) {
-        case "https://i.postimg.cc/LXcFfSF2/Squirrel.png":
-            imageChange.setAttribute("src", "https://i.postimg.cc/NG7Bj51Q/Rumcajs1.jpg");
-        break;
-    }
-});
+    const addingDecoration = () => {
+        const decoration = document.querySelector(".js-sectionDecoration");
+        decoration.classList.add("lineDecoration");
+    };
 
-buttonSwitchMode.addEventListener("click", () => {
-    body.classList.toggle("bodyLightTheme");
-    background.classList.toggle("backgroundLightTheme");
-    headline.forEach((element) => { element.classList.toggle("headlineLightTheme") });
-    tableHeader.forEach((element) => { element.classList.toggle("tableHeaderLightTheme") });
-    tableCellBackground.forEach((element) => { element.classList.toggle("tableCellLightTheme") });
-    hyperlink.forEach((element) => { element.classList.toggle("hyperlinkLightTheme") });
-    endText.classList.toggle("endTextLightTheme");
-    buttonSwitchMode.classList.toggle("buttonLightTheme");
-    buttonHideSquirrel.classList.toggle("buttonLightTheme");
+    const addingText = () => {
+        const catName = document.querySelector(".js-paragraphCat");
+        catName.innerText = "Przyszywany brat - Rumcajs. Postrach wiewiórek!";
+    };
 
-    buttonSwitchMode.innerText = body.classList.contains("bodyLightTheme")
-        ? "Ustaw ciemny motyw"
-        : "Ustaw jasny motyw";
-});
+    const hidingSquirrelImage = (buttonHideSquirrel) => {
+        buttonHideSquirrel.remove();
+
+        addingText();
+        addingDecoration();
+        imageChanging();
+    };
+
+    const switchingButtonText = (body, buttonSwitchMode) => {
+        buttonSwitchMode.innerText = body.classList.contains("bodyLightTheme")
+            ? "Ustaw ciemny motyw"
+            : "Ustaw jasny motyw";
+    };
+
+    const changingFooterMode = () => {
+        const endText = document.querySelector(".js-footerText");
+        endText.classList.toggle("endTextLightTheme");
+    };
+
+    const changingHyperlinksMode = () => {
+        const hyperlink = document.querySelectorAll(".js-hyperlink");
+        hyperlink.forEach((element) => {
+            element.classList.toggle("hyperlinkLightTheme");
+        });
+    };
+
+    const changingHeadersMode = () => {
+        const headline = document.querySelectorAll(".js-sectionHeadline");
+        const tableHeader = document.querySelectorAll(".js-tableHeader");
+
+        headline.forEach((element) => {
+            element.classList.toggle("headlineLightTheme");
+        });
+        tableHeader.forEach((element) => {
+            element.classList.toggle("tableHeaderLightTheme");
+        });
+    };
+
+    const changingButtonsMode = (buttonSwitchMode, buttonHideSquirrel) => {
+        buttonSwitchMode.classList.toggle("buttonLightTheme");
+        buttonHideSquirrel.classList.toggle("buttonLightTheme");
+    };
+
+    const toggleLightMode = (buttonSwitchMode, buttonHideSquirrel) => {
+        const body = document.querySelector(".js-body");
+        const background = document.querySelector(".js-background");
+        const tableCellBackground = document.querySelectorAll(".js-tableCell");
+
+        body.classList.toggle("bodyLightTheme");
+        background.classList.toggle("backgroundLightTheme");
+        tableCellBackground.forEach((element) => {
+            element.classList.toggle("tableCellLightTheme");
+        });
+
+        changingButtonsMode(buttonSwitchMode, buttonHideSquirrel);
+        changingHeadersMode();
+        changingHyperlinksMode();
+        changingFooterMode();
+        switchingButtonText(body, buttonSwitchMode);
+    };
+
+    const init = () => {
+        const buttonHideSquirrel = document.querySelector(
+            ".js-buttonHideSquirrel"
+        );
+        const buttonSwitchMode = document.querySelector(".js-buttonSwitchMode");
+
+        buttonHideSquirrel.addEventListener("click", () =>
+            hidingSquirrelImage(buttonHideSquirrel)
+        );
+        buttonSwitchMode.addEventListener("click", () =>
+            toggleLightMode(buttonSwitchMode, buttonHideSquirrel)
+        );
+        welcome();
+    };
+
+    init();
+}
